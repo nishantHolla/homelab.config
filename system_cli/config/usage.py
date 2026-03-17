64fb8cfe3e97512476071dc3e1289afb3cfd11e1
Nishant Holla <nishantholla19@gmail.com>
2026-03-18 00:54:26 +0530

    system_cli: Add service subcommand

--- system_cli/config/usage.py
+++ system_cli/config/usage.py
@@ -41,4 +41,19 @@ sub-command:
     help:
 
         Print help message and exit.
+
+    check <service_name>:
+
+        Check if a service is configured correctly.
+        Pass empty service name to check all services.
+
+    up <service_name>:
+
+        Compose up a service.
+        Pass empty service name to compose up all services.
+
+    down <service_name>:
+
+        Compose down a service.
+        Pass empty service name to compose down all services.
 """
