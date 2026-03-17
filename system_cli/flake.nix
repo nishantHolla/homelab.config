23787d30e34d52cd7f90c99d0a3d746d8a8665f2
Nishant Holla <nishantholla19@gmail.com>
2026-03-17 19:24:12 +0530

    system_cli: Fix nix shell

--- system_cli/flake.nix
+++ system_cli/flake.nix
@@ -1,20 +1,20 @@
 {
-  description = "Nix Shell for system_cli in Homelab";
-
+  description = "Shell for system_cli of Homelab";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs }:
-    let
-      system = builtins.currentSystem;
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = [
-          pkgs.python313
-          pkgs.python313Packages.docker
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.python313
+            pkgs.python313Packages.docker
+          ];
+        };
+      }
+    );
 }
