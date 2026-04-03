905fd6c51058864f9af232c0d5d57215c276302f
Nishant Holla <nishantholla19@gmail.com>
2026-04-03 10:08:54 +0530

    homelab_cli: Update values

--- homelab_cli/config/values.py
+++ homelab_cli/config/values.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 ## Paths
 
-SYSTEM_CLI_DIR = Path(__file__).parent.parent
-SYSTEM_DIR = SYSTEM_CLI_DIR.parent
+HOMELAB_CLI_DIR = Path(__file__).parent.parent
+HOMELAB_DIR = HOMELAB_CLI_DIR.parent
 
-NIXOS_DIR = SYSTEM_DIR / "nixos"
+NIXOS_DIR = HOMELAB_DIR / "nixos"
 NIXOS_TEMPLATE_DIR = NIXOS_DIR / "template"
 NIXOS_FLAKE_FILE = NIXOS_DIR / "flake.nix"
 
-SERVICE_DIR = SYSTEM_DIR / "service"
-SECRET_DIR = SYSTEM_DIR / "secrets"
+SERVICE_DIR = HOMELAB_DIR / "service"
+SECRET_DIR = HOMELAB_DIR / "secrets"
