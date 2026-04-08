04f37d2113c85081903a638a3a8ef71f49954665
Nishant Holla <nishantholla19@gmail.com>
2026-04-08 22:40:34 +0530

    homelab_cli: Fix dynamic completion

--- homelab_cli/commands/completion.py
+++ homelab_cli/commands/completion.py
@@ -3,6 +3,6 @@ from config import values as v
 import typer
 
 
-def complete_services(incomplete: str) -> List[str]:
+def complete_services(ctx, param, incomplete: str) -> List[str]:
     services: List[str] = [item.name for item in v.SERVICE_DIR.iterdir()]
     return [s for s in services if s.startswith(incomplete)]
