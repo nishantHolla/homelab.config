c33a86dbc49bb5afebe4efd2c522e371f6225204
Nishant Holla <nishantholla19@gmail.com>
2026-09-01 18:11:13 +0530

    nixos: Remove backup config

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -33,7 +33,6 @@
         ./homelab/config.nix
         ./homelab/packages.nix
         ./homelab/hardware.nix
-        ./homelab/backup.nix
 
         ./homelab/service/convertx.nix
         ./homelab/service/excalidraw.nix
